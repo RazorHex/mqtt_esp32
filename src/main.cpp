1b9becc71d365032d6f6933fdb129154491da4ba
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-10-25 15:08:47 +0700

    Adding DHT22 code

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,19 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include <AntaresESPMQTT.h>
+#include <DHTesp.h>
 
-#define SSID "AHDA 4434"
-#define PASS "Ahda@hotspot"
+#define SSID "MBC-Lab 2.4G"
+#define PASS "gogombc123"
 #define mqtt_server "mqtt.antares.id" // Using antares mqtt broker
 
 #define ACCESSKEY "5857af4ec372ee95:d546da27532c4a59" // Antares account access key
 #define projectName "tracker"                         // Name of the application created in Antares
 #define deviceName "esp32"                            // Name of the device created in Antares
 
+const int dht = 13;
+
+DHTesp dhtsensor;
 AntaresESPMQTT antares(ACCESSKEY);
 
 void publish()
@@ -18,8 +22,11 @@ void publish()
   Serial.println("\n====");
   int temp = random(25, 30);
   int hum = random(75, 90);
-  antares.add("temperature", temp);
-  antares.add("humidity", hum);
+
+  TempAndHumidity data = dhtsensor.getTempAndHumidity();
+
+  antares.add("temperature", data.temperature);
+  antares.add("humidity", data.humidity);
   antares.publish(projectName, deviceName);
   Serial.println("=== Published ===\n");
 }
@@ -38,7 +45,10 @@ void setup()
   Serial.begin(115200);
   delay(500);
 
-  Serial.println("\nConncting to antares MQTT..");
+  Serial.println("\n[DHT-22] Prepairing DHT sensor...");
+  dhtsensor.setup(dht, DHTesp::DHT22);
+  Serial.println("\n[DHT-22] Done.");
+
   antares.setDebug(true);
   antares.wifiConnection(SSID, PASS);
   antares.setMqttServer();
@@ -55,5 +65,5 @@ void loop()
 
   delay(500);
   antares.retrieveLastData(projectName, deviceName);
-  delay(15000);
+  delay(10000);
 }
